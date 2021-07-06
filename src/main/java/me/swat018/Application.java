b88114f6eebfec2a58163ade95c622eb7bd2ef63
jinwoo park <swat018@icloud.com>
2021-07-07 00:00:00 +0900

    Initial commit

---
+++ src/main/java/me/swat018/Application.java
@@ -0,0 +1,12 @@
+package me.swat018;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringBootApplication.run(Application.class, args);
+    }
+}
