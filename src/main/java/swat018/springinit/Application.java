592ad3c3f9b0a231732907acbe8f1de57bd90349
jinwoo park <swat018@gmail.com>
2021-07-11 00:40:35 +0900

    자동 설정 개요

--- src/main/java/swat018/springinit/Application.java
+++ src/main/java/swat018/springinit/Application.java
@@ -1,12 +1,23 @@
 package swat018.springinit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@SpringBootConfiguration
+//@ComponentScan
+//@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
+//        SpringApplication application = new SpringApplication(Application.class);
+//        application.setWebApplicationType(WebApplicationType.NONE);
+//        application.run(args);
+
         SpringApplication.run(Application.class, args);
     }
 }
