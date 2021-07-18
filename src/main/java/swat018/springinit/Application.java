80372310f501f3eba2af808be9e1dfc3ce05be53
jinwoo park <swat018@gmail.com>
2021-07-19 02:01:44 +0900

    내장 웹 서버 응용 1부: 컨테이너와 서버 포트

--- src/main/java/swat018/springinit/Application.java
+++ src/main/java/swat018/springinit/Application.java
@@ -1,42 +1,12 @@
 package swat018.springinit;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-
+@SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws LifecycleException {
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-
-        Context context = tomcat.addContext("/", "/");
-
-        HttpServlet servlet = new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                PrintWriter writer = resp.getWriter();
-                writer.println("<html><head><title>");
-                writer.println("Hey, Tomcat");
-                writer.println("</title></head");
-                writer.println("<body><h1>Hello Tomcat</h1></body>");
-                writer.println("</html>");
-            }
-        };
-
-        String servletName = "helloServlet";
-        tomcat.addServlet("/", servletName, servlet);
-        context.addServletMapping("/hello", servletName);
-
-        tomcat.start();
-        tomcat.getServer().await();
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
