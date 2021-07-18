b592c023acbc1b064bd9fdfa3e0c207cfc4780ed
jinwoo park <swat018@gmail.com>
2021-07-18 22:13:26 +0900

    내장 웹 서버 이해

--- src/main/java/swat018/springinit/Application.java
+++ src/main/java/swat018/springinit/Application.java
@@ -1,18 +1,42 @@
 package swat018.springinit;
 
-import me.jinwoo.Holoman;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
-    }
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
 
+        Context context = tomcat.addContext("/", "/");
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println("<html><head><title>");
+                writer.println("Hey, Tomcat");
+                writer.println("</title></head");
+                writer.println("<body><h1>Hello Tomcat</h1></body>");
+                writer.println("</html>");
+            }
+        };
+
+        String servletName = "helloServlet";
+        tomcat.addServlet("/", servletName, servlet);
+        context.addServletMapping("/hello", servletName);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
 }
