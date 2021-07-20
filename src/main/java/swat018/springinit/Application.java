678574518b9830fe871a0e0b362310238f79cb45
jinwoo park <swat018@gmail.com>
2021-07-20 23:56:13 +0900

    내장 웹 서버 응용 2부: HTTPS와 HTTP2 수정

--- src/main/java/swat018/springinit/Application.java
+++ src/main/java/swat018/springinit/Application.java
@@ -1,11 +1,7 @@
 package swat018.springinit;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
