7934a37667937aa8d52e30b2f4647c327938d835
jinwoo park <swat018@gmail.com>
2021-07-22 23:53:03 +0900

    독립적으로 실행 가능한 JAR

--- src/main/java/swat018/springinit/Application.java
+++ src/main/java/swat018/springinit/Application.java
@@ -18,17 +18,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    public ServletWebServerFactory servletContainer() {
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-//        tomcat.addAdditionalTomcatConnectors(createStandardardConnector());
-//        return tomcat;
-//    }
-//
-//    private Connector createStandardardConnector() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setPort(0);
-//        return connector;
-//    }
-
 }
