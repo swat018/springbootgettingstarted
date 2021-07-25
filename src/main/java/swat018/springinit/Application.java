e9e431bcc98d4c9f8d3c92387224e16322d72801
jinwoo park <swat018@gmail.com>
2021-07-25 10:21:19 +0900

    스프트 부트 활용 시작 스프링 부트 핵심 기능 SpringApplication 1부

--- src/main/java/swat018/springinit/Application.java
+++ src/main/java/swat018/springinit/Application.java
@@ -1,21 +1,31 @@
 package swat018.springinit;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello spring";
-    }
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+//        SpringApplication app = new SpringApplication(Application.class);
+//        app.setBanner(new Banner() {
+//            @Override
+//            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+//                out.println("=======================");
+//                out.println("JinWoo");
+//                out.println("=======================");
+//            }
+//        });
+////        app.setBannerMode(Banner.Mode.OFF);
+//        app.run(args);
 
+        new SpringApplicationBuilder()
+                .sources(Application.class)
+                .run(args);
+    }
 }
