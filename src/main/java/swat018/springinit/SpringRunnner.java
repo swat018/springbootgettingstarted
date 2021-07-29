3eafae92d358b702a253b187d83b20cbceaa5823
jinwoo park <swat018@gmail.com>
2021-07-30 07:43:22 +0900

    로깅 1부: 스프링 부트 기본 로거 설정

--- src/main/java/swat018/springinit/SpringRunnner.java
+++ src/main/java/swat018/springinit/SpringRunnner.java
@@ -1,5 +1,7 @@
 package swat018.springinit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringRunnner implements ApplicationRunner {
+
+    private Logger logger = LoggerFactory.getLogger(SpringRunnner.class);
 
     @Autowired
     private String hello;
@@ -16,10 +20,10 @@ public class SpringRunnner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("=======================");
-        System.out.println(hello);
-        System.out.println(jinwooProperties.getName());
-        System.out.println(jinwooProperties.getFullName());
-        System.out.println("=======================");
+        logger.info("=======================");
+        logger.info(hello);
+        logger.info(jinwooProperties.getName());
+        logger.info(jinwooProperties.getFullName());
+        logger.info("=======================");
     }
 }
